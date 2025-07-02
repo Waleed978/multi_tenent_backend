c3a95d5ce7e1efdc90efc5099bff2aa8f6616ce6
Waleed978 <waleed.tariq@invicttus.com>
2025-07-02 15:17:42 +0500

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Waleed978/multi_tenent_backend
+
+go 1.24.4
+
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	godotenv.Load()
+	port := os.Getenv("PORT")
+	fmt.Println(port)
+	router := chi.NewRouter()
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+	http.ListenAndServe(":"+port, router)
+}
